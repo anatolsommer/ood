9406a35330a149bb94f6978c7f83612220be0150
anatolsommer <anatol@anatol.at>
2016-02-19 20:57:38 +0100

    improved autocomplete

--- templates/autocomplete.sh
+++ templates/autocomplete.sh
@@ -4,8 +4,8 @@ _ood ()
 
   COMPREPLY=()
   cur=${COMP_WORDS[COMP_CWORD]}
-  cmdp='@(init|start|stop|restart|scale|redirect|status|config|ssl|log|help)'
-  cmdstr='init start stop restart scale redirect status config ssl log help'
+  cmdp='@(init|start|stop|restart|scale|redirect|status|config|ssl|mod|log|help)'
+  cmdstr='init start stop restart scale redirect status config ssl mod log help'
 
 
   for (( i=0; i < ${#COMP_WORDS[@]}-1; i++ )); do
@@ -20,30 +20,60 @@ _ood ()
   if [ -n "$cmd" ]; then
     case $cmd in
       init)
-        COMPREPLY=( $( compgen -W '--help --script --cwd --alias' -- $cur ) );;
+        if [ -n "$arg" ] && [[ $cur != -* ]] ; then
+          case $arg in
+            --script|-s)
+              COMPREPLY=( $( compgen -W "$(ls *.js)" -- $cur ) );;
+            --cwd|-d)
+              compopt -o nospace;
+              if [[ $cur == "" ]] ; then
+                cur="/"
+              fi
+              COMPREPLY=( $( compgen -S "/" -d "$cur" ) );;
+          esac
+        else
+          COMPREPLY=( $( compgen -W '--help --script --cwd --alias' -- $cur ) );
+        fi
+        ;;
       start|stop|restart|scale|status)
         COMPREPLY=( $( compgen -W "$(ood autocomplete app $cmd)" -- $cur ) );;
       config)
         if [ -n "$arg" ] && [[ $cur != -* ]] ; then
           case $arg in
             --app|-ga)
-              COMPREPLY=( $( compgen -W "$(ood autocomplete app)" -- $cur ) );
-              return 0;;
+              COMPREPLY=( $( compgen -W "$(ood autocomplete app)" -- $cur ) );;
           esac
+        else
+          COMPREPLY=( $( compgen -W '--help --app --get --set --delete -ga' -- $cur ) );
         fi
-        COMPREPLY=( $( compgen -W '--help --app --get --set --delete -ga' -- $cur ) );;
+        ;;
       ssl)
         if [ -n "$arg" ] && [[ $cur != -* ]] ; then
           case $arg in
+            --import|-i)
+              compopt -o nospace;
+              COMPREPLY=( $( compgen -f "$cur" ) );;
+            --auto|-a)
+              COMPREPLY=( $( compgen -W "$(ood autocomplete app)" -- $cur ) );;
             --delete-ca)
-              COMPREPLY=( $( compgen -W "$(ood autocomplete ssl ca)" -- $cur ) );
-              return 0;;
+              COMPREPLY=( $( compgen -W "$(ood autocomplete ssl ca)" -- $cur ) );;
             --delete)
-              COMPREPLY=( $( compgen -W "$(ood autocomplete ssl cert)" -- $cur ) );
-              return 0;;
+              COMPREPLY=( $( compgen -W "$(ood autocomplete ssl cert)" -- $cur ) );;
           esac
+        else
+          COMPREPLY=( $( compgen -W '--help --import --auto --email --agree --delete --delete-ca --list' -- $cur ) );
         fi
-        COMPREPLY=( $( compgen -W '--help --auto --email --agree --delete --delete-ca --list -l' -- $cur ) );;
+        ;;
+      mod)
+        if [ -n "$arg" ] && [[ $cur != -* ]] ; then
+          case $arg in
+            --enable|-e|--disable|-d)
+              COMPREPLY=( $( compgen -W "$(ood autocomplete mod _$arg)" -- $cur ) );;
+          esac
+        else
+          COMPREPLY=( $( compgen -W '--help --enable --disable --install' -- $cur ) );
+        fi
+        ;;
       help)
         COMPREPLY=( $( compgen -W "$cmdstr" -- $cur ) );;
     esac
